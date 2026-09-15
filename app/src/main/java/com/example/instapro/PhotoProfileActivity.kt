85ae890ce61e5b21d8e41a71e5cc1118fc36bab8
Atalariq Barra Hadinugraha <atalariq.dev@outlook.com>
2026-09-15 08:33:03 +0700

    feat(activity): edit profile and view photo profile

---
+++ app/src/main/java/com/example/instapro/PhotoProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.example.instapro
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PhotoProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_profile)
+    }
+}
